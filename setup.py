ac9c82a3c8ddad56157c15ff8ea326d76f88e483
Mike Boers <github@mikeboers.com>
2014-07-01 18:02:58 +0100

    Bump to 1.1.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Images',
-    version='1.1.4',
+    version='1.1.5',
     description='Dynamic image resizing for Flask.',
     url='http://github.com/mikeboers/Flask-Images',
         
