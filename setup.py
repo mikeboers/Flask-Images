79e870ca65885942deec6a6605b4053201d2ee66
Nick Baldallo <nnickbaldallo@gmail.com>
2023-10-19 21:23:10 +0100

    replace ANTIALIAS with LANCZOS

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
 
     name='Flask-Image-Resizer',
-    version='3.0.5',
+    version='3.0.6',
     description='Dynamic image resizing for Flask.',
     url='http://github.com/mikeboers/Flask-Images',
         
